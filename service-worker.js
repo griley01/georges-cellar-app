e3fd341733d88fd6994f307727d3afc9a4f037fb
griley01 <68755366+griley01@users.noreply.github.com>
2026-09-15 23:16:47 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'georges-cellar-v2';
+const CACHE_NAME = 'georges-cellar-v3';
 const APP_SHELL = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', function (event) {
